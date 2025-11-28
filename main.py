11cf4de327830ba21d4230f4f687e225a8a15f55
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-11-28 11:04:38 +0100

    Changed generation a bit

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ client = genai.Client(api_key=os.environ['GEMINI_API_KEY'])
 
 
 class CandleQuestion(BaseModel):
+    idea_inferred: str = Field(
+        description="Idea that can be inferred from the random seed, it doesn't have to be the idea of the text itself but it may cause you to think of something that may help generate an interesting question"
+    )
     question_explanation: str = Field(
         description="Short explanation of what the question aims to explore."
     )
@@ -18,25 +21,24 @@ class CandleQuestion(BaseModel):
     )
 
 
-for i in range(5):
-    for _ in range(5):
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=prompt.build_prompt(),
-            config={
-                "response_mime_type": "application/json",
-                "response_json_schema": CandleQuestion.model_json_schema(),
-            },
-        )
+for _ in range(5):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt.build_prompt(),
+        config={
+            "response_mime_type": "application/json",
+            "response_json_schema": CandleQuestion.model_json_schema(),
+        },
+    )
 
-        text = (response.text or "").strip()
-        print(text)
+    text = (response.text or "").strip()
+    print(text)
 
-        obj = CandleQuestion.model_validate_json(text)
-        question = obj.question
-        explanation = obj.question_explanation
+    obj = CandleQuestion.model_validate_json(text)
+    question = obj.question
+    explanation = obj.question_explanation
 
-        db.insert_question(
-            question_body=question,
-            question_explanation=explanation,
-        )
+    db.insert_question(
+        question_body=question,
+        question_explanation=explanation,
+    )
--- prompt.py
+++ prompt.py
@@ -5,17 +5,10 @@ import random_string
 def build_prompt():
     prompt = f"""
 Your job is to generate questions simmilar in style and purpose as the candle mobile application and its competitors.
-The questions should not be repetitive or similar to the ones that have been generated earlier, to avoid that you are provided with some previously generated questions avoid repeating the same topics, wording, ideas, concepts etc.:
+The questions should not be repetitive or similar to the ones that have been generated earlier, to avoid that you are provided with some previously generated questions avoid repeating the same topics, wording, ideas, concepts, question type, etc. Analyse the questions that are given to you and find what is common between them and avoid whatever that is. Additionally to the common features, the new question should not repeat any individual question neither:
 {db.get_recent_questions(10)}
 
 As well as that, knowing that you are an ai model that tends to generate repetitive content, you are introduced with a source of randomness, a paragraph from a romanian story. It will allow you to infer some context or a distant idea that shouldn't be present in the question itself but you can develop it into something that can be used in a Candle-style question:
 {random_string.get_random_paragraph()}
-
-The length of the question should be limited to 25 words.
-Output format:
-{{
-    "question_explanation": "<explanation of the topic that the question wants to address, limited to 50 words>",
-    "question": "<question, limited to 25 words>"
-}}
     """
     return prompt
